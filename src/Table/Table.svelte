f7edcf63f422389ca1813c228ce19c533118f57e
kassimadsen <madsenkassi@gmail.com>
2024-12-14 21:07:54 -0500

    fixed using the same function for all the tally's

--- src/Table/Table.svelte
+++ src/Table/Table.svelte
@@ -7,11 +7,61 @@
 		TableHead,
 		TableHeadCell
 	} from 'flowbite-svelte';
-	let tally = [];
-	let count = 0;
-	function appendTally() {
-		tally.push(count++);
-		tally = tally;
+	
+	let mondaytally = [];
+	let mondaycount = 0;
+
+	let tuesdaytally = [];
+	let tuesdaycount = 0;
+
+	let wednesdaytally = [];
+	let wednesdaycount = 0;
+	
+	let thursdaytally = [];
+	let thursdaycount = 0;
+
+	let fridaytally = [];
+	let fridaycount = 0;
+
+	let saturdaytally = [];
+	let saturdaycount = 0;
+
+	let sundaytally = [];
+	let sundaycount = 0;
+	
+	function appendMondayTally() {
+		mondaytally.push(mondaycount++);
+		mondaytally = mondaytally;
+	}
+
+	function appendTuesdayTally() {
+		tuesdaytally.push(tuesdaycount++);
+		tuesdaytally = tuesdaytally;
+	}
+
+	function appendWednesdayTally() {
+		wednesdaytally.push(wednesdaycount++);
+		wednesdaytally = wednesdaytally;
+	}
+
+	function appendThursdayTally() {
+		thursdaytally.push(thursdaycount++);
+		thursdaytally = thursdaytally;
+	}
+
+	function appendFridayTally() {
+		fridaytally.push(fridaycount++);
+		fridaytally = fridaytally;
+	}
+
+	function appendSaturdayTally() {
+		saturdaytally.push(saturdaycount++);
+		saturdaytally = saturdaytally;
+	}
+
+	function appendSundayTally() {
+		sundaytally.push(sundaycount++);
+		sundaytally = sundaytally;
 	}
 </script>
 
@@ -28,58 +78,58 @@
 	<TableBody tableBodyClass="divide-y">
 		<TableBodyRow>
 			<TableBodyCell>
-				<button id="tally" on:click={appendTally}>Add Tally</button>
-				<ol id="count">
-					{#each tally as i}
-						<li class="tally"></li>
+				<button id="mondaytally" on:click={appendMondayTally}>Add Monday Tally</button>
+				<ol id="mondaycount">
+					{#each mondaytally as i}
+						<li class="monday"></li>
 					{/each}
 				</ol>
 			</TableBodyCell>
 			<TableBodyCell>
-				<button id="tally" on:click={appendTally}>Add Tally</button>
-				<ol id="count">
-					{#each tally as i}
-						<li class="tally"></li>
+				<button id="tuesdaytally" on:click={appendTuesdayTally}>Add Tuesday Tally</button>
+				<ol id="tuesdaycount">
+					{#each tuesdaytally as i}
+						<li class="tuesday"></li>
 					{/each}
 				</ol>
 			</TableBodyCell>
 			<TableBodyCell>
-				<button id="tally" on:click={appendTally}>Add Tally</button>
-				<ol id="count">
-					{#each tally as i}
-						<li class="tally"></li>
+				<button id="wednesdaytally" on:click={appendWednesdayTally}>Add Wednesday Tally</button>
+				<ol id="wednesdaycount">
+					{#each wednesdaytally as i}
+						<li class="wednesday"></li>
 					{/each}
 				</ol>
 			</TableBodyCell>
 			<TableBodyCell>
-				<button id="tally" on:click={appendTally}>Add Tally</button>
-				<ol id="count">
-					{#each tally as i}
-						<li class="tally"></li>
+				<button id="thursdaytally" on:click={appendThursdayTally}>Add Thursday Tally</button>
+				<ol id="thursdaycount">
+					{#each thursdaytally as i}
+						<li class="thursday"></li>
 					{/each}
 				</ol>
 			</TableBodyCell>
 			<TableBodyCell>
-				<button id="tally" on:click={appendTally}>Add Tally</button>
-				<ol id="count">
-					{#each tally as i}
-						<li class="tally"></li>
+				<button id="fridaytally" on:click={appendFridayTally}>Add Friday Tally</button>
+				<ol id="fridaycount">
+					{#each fridaytally as i}
+						<li class="friday"></li>
 					{/each}
 				</ol>
 			</TableBodyCell>
 			<TableBodyCell>
-				<button id="tally" on:click={appendTally}>Add Tally</button>
-				<ol id="count">
-					{#each tally as i}
-						<li class="tally"></li>
+				<button id="saturdaytally" on:click={appendSaturdayTally}>Add Saturday Tally</button>
+				<ol id="saturdaycount">
+					{#each saturdaytally as i}
+						<li class="saturday"></li>
 					{/each}
 				</ol>
 			</TableBodyCell>
 			<TableBodyCell>
-				<button id="tally" on:click={appendTally}>Add Tally</button>
-				<ol id="count">
-					{#each tally as i}
-						<li class="tally"></li>
+				<button id="sundaytally" on:click={appendSundayTally}>Add Sunday Tally</button>
+				<ol id="sundaycount">
+					{#each sundaytally as i}
+						<li class="sunday"></li>
 					{/each}
 				</ol>
 			</TableBodyCell>
